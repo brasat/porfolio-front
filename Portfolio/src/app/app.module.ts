f9fce44dfe5cb9be83f303136ae2cfb539e4925e
br32 <brunito23245@gmail.com>
2022-08-14 19:24:39 -0300

    second commit

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { AcercaDeComponent } from './component/acerca-de/acerca-de.component';
+import { HomeComponent } from './component/home/home.component';
+import { AptitudesComponent } from './component/aptitudes/aptitudes.component';
+import { ExperienciaYEducacionComponent } from './component/experiencia-y-educacion/experiencia-y-educacion.component';
+import { IniciarSesionComponent } from './component/iniciar-sesion/iniciar-sesion.component';
+import { LoginComponent } from './component/login/login.component';
+import { LogrosComponent } from './component/logros/logros.component';
+import { PortfolioComponent } from './component/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    HomeComponent,
+    AptitudesComponent,
+    ExperienciaYEducacionComponent,
+    IniciarSesionComponent,
+    LoginComponent,
+    LogrosComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
