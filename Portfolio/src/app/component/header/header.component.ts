d1f139b05b34248604ab85753c3a53269019d836
br32 <brunito23245@gmail.com>
2022-08-15 21:34:13 -0300

    service portfolio

--- Portfolio/src/app/component/header/header.component.ts
+++ Portfolio/src/app/component/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {PortfolioService} from 'src/app/service/portfolio.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  myPortfolio:any;
+  constructor(private datosPortfolio:PortfolioService) { }
 
   ngOnInit(): void {
+    this.datosPortfolio.ObtenerDatos().subscribe(data => {
+      this.myPortfolio = data;
+    });
   }
 
 }
